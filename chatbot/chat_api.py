6aef6e03d345ebb40ef22be01f4e7548cfb32ba0
tobiask42 <tobiaskoch93@yahoo.de>
2025-10-01 17:39:36 +0200

    removed redundant call of settings singleton from lambda

--- chatbot/chat_api.py
+++ chatbot/chat_api.py
@@ -38,7 +38,7 @@ def home():
 class AskReq(BaseModel):
     question: str
     timeout_s: int = Field(
-        default_factory=lambda: get_settings().OLLAMA_TIMEOUT_S, ge=1, le=300) # Fall-Back für Timeout
+        default_factory=lambda: custom_settings.OLLAMA_TIMEOUT_S, ge=1, le=300) # Fall-Back für Timeout
 
 @app.post("/chat/ask")
 async def chat_ask(request: AskReq) -> dict[str, Any]:
